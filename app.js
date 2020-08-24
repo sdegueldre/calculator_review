e4c4d1fd4b56faaf42a952c6708b486cde977d14
Leinier Perez <P_Leinier@yahoo.com>
2020-08-23 23:54:01 -0400

    Refactor code once again and created function to store the numbers for the operation

--- app.js
+++ app.js
@@ -38,9 +38,103 @@ function operate(operator, a, b) {
 const display = document.querySelector(".display");
 const btns = document.querySelectorAll("button");
 
-// put number selected on the display
 btns.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    display.innerHTML += button.innerText;
-  });
+  button.addEventListener("click", (e) => calculatorDisplay(button.id));
 });
+
+let firstNum = "";
+let secondNum = "";
+let operator;
+// put number selected on the display
+function calculatorDisplay(value) {
+  function getData(value) {
+    if (
+      display.innerHTML.includes("-") ||
+      display.innerHTML.includes("+") ||
+      display.innerHTML.includes("x") ||
+      display.innerHTML.includes("÷")
+    ) {
+      secondNum += value;
+    } else {
+      firstNum += value;
+    }
+  }
+
+  switch (value) {
+    case "zero":
+      display.innerHTML += "0";
+      getData("0");
+      break;
+    case "one":
+      display.innerHTML += "1";
+      getData("1");
+      break;
+    case "two":
+      display.innerHTML += "2";
+      getData("2");
+      break;
+    case "three":
+      display.innerHTML += "3";
+      getData("3");
+      break;
+    case "four":
+      display.innerHTML += "4";
+      getData("4");
+      break;
+    case "five":
+      display.innerHTML += "5";
+      getData("5");
+      break;
+    case "six":
+      display.innerHTML += "6";
+      getData("6");
+      break;
+    case "seven":
+      display.innerHTML += "7";
+      getData("7");
+      break;
+    case "eight":
+      display.innerHTML += "8";
+      getData("8");
+      break;
+    case "nine":
+      display.innerHTML += "9";
+      getData("9");
+      break;
+    case "decimal":
+      display.innerHTML += ".";
+      getData(".");
+      break;
+    case "clear":
+      display.innerHTML = "";
+      firstNum = "";
+      secondNum = "";
+      operator = undefined;
+      break;
+    case "minus":
+      display.innerHTML += "-";
+      operator = "subtract";
+      break;
+    case "plus":
+      display.innerHTML += "+";
+      operator = "add";
+      break;
+    case "times":
+      display.innerHTML += "x";
+      operator = "multiply";
+      break;
+    case "divide":
+      display.innerHTML += "÷";
+      operator = "divide";
+      break;
+    case "equals":
+      console.log(firstNum);
+      console.log(secondNum);
+      display.innerHTML = operate(
+        operator,
+        Number(firstNum),
+        Number(secondNum)
+      );
+      break;
+  }
+}
